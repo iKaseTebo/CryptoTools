c17fdcb7f8a9aac75c2738dbad4b9720a9d8c8c8
Kase Tebo <iKaseTebo@gmail.com>
2017-10-05 13:23:50 -0400

    Font Awesome

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,43 @@
-myApp.controller('indexController', ['$scope', function ($scope) {
+myApp.controller('indexController', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
     $scope.index = 'Welcome to the Crypto Index';
+    $scope.btcExchange = '';
+    $http.get("/btc/").then(successCallback, errorCallback);
+    setInterval(function(){
+        $http.get("/btc/").then(successCallback, errorCallback)
+    },5000);
 
+    $scope.convert = function($i){
+        var usd = $scope.usdExchange;
+        var btc = $scope.btcExchange;
+        if($i === 1){
+            $scope.usdExchange = $filter('currency')($scope.market*btc, '$', 2);
+            if(!btc && btc !== 0){
+                $scope.btcExchange = '';
+                $scope.usdExchange = '';
+            }
+        }else if($i === 2){
+            var total = usd;
+            total = total.replace('$', '');
+            total = total.replace(',', '');
+            $scope.btcExchange = round(total/$scope.market, 8);
+            if(!usd){
+                $scope.btcExchange = '';
+                $scope.usdExchange = '';
+            }
+        }
+
+    };
+
+
+    function successCallback(result){
+        $scope.market = result.data;
+    }
+    function errorCallback(data, status){
+        console.log(data);
+    }
+
+    function round(value, decimals) {
+        return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+    }
 }]);
 //# sourceMappingURL=controllers.js.map
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +19,12 @@ Route::get('/', function () {
 
 Route::get('/crypto', function () {
     return view('crypto.home');
+});
+
+Route::get('/btc', function(){
+    $client = new Client();
+    $result = $client->request('GET', 'https://bittrex.com/api/v1.1/public/getmarketsummary?market=usdt-btc');
+    $body = json_decode($result->getBody());
+    $value = $body->result[0]->Last;
+    return $value;
 });
